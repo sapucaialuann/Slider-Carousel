97e77a4b0d71b542ff79163d6a1af812a3b20e61
Luann Sapucaia <luannsapucaia@gmail.com>
2020-05-24 00:31:18 -0300

    Updates

---
+++ precache-manifest.0aca9e28709ce5546c9500b1e27054cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ef53e13fe486a5d56bed1457af8e06",
+    "url": "/Slider-Carousel/index.html"
+  },
+  {
+    "revision": "8d63c6cee931be601ee5",
+    "url": "/Slider-Carousel/static/css/main.92b7c444.chunk.css"
+  },
+  {
+    "revision": "27bf27de130aa92b0921",
+    "url": "/Slider-Carousel/static/js/2.720205c1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Slider-Carousel/static/js/2.720205c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d63c6cee931be601ee5",
+    "url": "/Slider-Carousel/static/js/main.3773515f.chunk.js"
+  },
+  {
+    "revision": "4243a68076ea02e9b4bc",
+    "url": "/Slider-Carousel/static/js/runtime-main.99b740f0.js"
+  },
+  {
+    "revision": "5c543d1ac57ff94e1456b09860f2709c",
+    "url": "/Slider-Carousel/static/media/dinosaur.5c543d1a.svg"
+  },
+  {
+    "revision": "f64f2f24d263e3a39089044ed2aedfb5",
+    "url": "/Slider-Carousel/static/media/dinosaur1.f64f2f24.svg"
+  },
+  {
+    "revision": "b6d30e734afdd3d5cff726422255caee",
+    "url": "/Slider-Carousel/static/media/dinosaur2.b6d30e73.svg"
+  },
+  {
+    "revision": "45c05624ccd3ab60e9c661d1ea9a5d93",
+    "url": "/Slider-Carousel/static/media/dinosaur3.45c05624.svg"
+  },
+  {
+    "revision": "e349b2e9a54920f171527b831778e5bb",
+    "url": "/Slider-Carousel/static/media/dinosaur4.e349b2e9.svg"
+  }
+]);
